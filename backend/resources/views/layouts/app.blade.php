eb0b3bce51b5d1031ed0c90b9a6ac3d9eb5204e5
bintangnugraha <djakarta1973@gmail.com>
2025-04-06 21:09:44 +0700

    Enhance HTML template: Add <title> tag and support for additional scripts

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+    <title>Ngekos</title>
 </head>
 
 <body>
@@ -18,6 +19,8 @@
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="{{ asset('assets/js/index.js') }}"></script>
+
+    @yield('script')
 </body>
 
 </html>
