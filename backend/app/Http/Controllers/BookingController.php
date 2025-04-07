ec6088b64c151c171c4e41f0ae0ed1ebe147f637
bintangnugraha <djakarta1973@gmail.com>
2025-04-07 12:37:17 +0700

    feat(booking): implement booking flow with session and information form

--- backend/app/Http/Controllers/BookingController.php
+++ backend/app/Http/Controllers/BookingController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CustomerInformationStoreRequest;
+use App\Interfaces\BoardingHouseRepositoryInterface;
+use App\Interfaces\TransactionRepositoryInterface;
 use Illuminate\Http\Request;
 
 /**
@@ -9,6 +12,30 @@ use Illuminate\Http\Request;
  */
 class BookingController extends Controller
 {
+    private BoardingHouseRepositoryInterface $boardingHouseRepository;
+    private TransactionRepositoryInterface $transactionRepository;
+
+    public function __construct(
+        BoardingHouseRepositoryInterface $boardingHouseRepository,
+        TransactionRepositoryInterface $transactionRepository
+    ) {
+        $this->boardingHouseRepository = $boardingHouseRepository;
+        $this->transactionRepository = $transactionRepository;
+    }
+
+    /**
+     * Handle booking request and store data in session.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param string $slug
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function booking(Request $request, $slug)
+    {
+        $this->transactionRepository->saveTransactionDataToSession($request->all());
+        return redirect()->route('booking.information', $slug);
+    }
+
     /**
      * Show the booking check page.
      *
@@ -17,5 +44,35 @@ class BookingController extends Controller
     public function check()
     {
         return view('pages.check-booking');
+    }
+
+    /**
+     * Show the booking information form.
+     *
+     * @param string $slug
+     * @return \Illuminate\View\View
+     */
+    public function information($slug)
+    {
+        $transaction = $this->transactionRepository->getTransactionDataFromSession();
+        $boardingHouse = $this->boardingHouseRepository->getBoardingHouseBySlug($slug);
+        $room = $this->boardingHouseRepository->getBoardingHouseRoomById($transaction['room_id']);
+
+        return view('pages.booking.information', compact('transaction', 'boardingHouse', 'room'));
+    }
+
+    /**
+     * Save customer information from the booking form.
+     *
+     * @param \App\Http\Requests\CustomerInformationStoreRequest $request
+     * @param string $slug
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function saveInformation(CustomerInformationStoreRequest $request, $slug)
+    {
+        $data = $request->validated();
+        $this->transactionRepository->saveTransactionDataToSession($data);
+
+        return redirect()->route('booking.checkout', $slug);
     }
 }
