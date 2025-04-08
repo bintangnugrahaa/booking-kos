4a143aae1e83afefd395ff9d8beb5300a390b66b
bintangnugraha <djakarta1973@gmail.com>
2025-04-08 22:43:48 +0700

    feat(interface): extend TransactionRepositoryInterface with saveTransaction method for final transaction saving

--- backend/app/Interfaces/TransactionRepositoryInterface.php
+++ backend/app/Interfaces/TransactionRepositoryInterface.php
@@ -10,15 +10,23 @@ interface TransactionRepositoryInterface
     /**
      * Get transaction data from session.
      *
-     * @return mixed
+     * @return array|null
      */
     public function getTransactionDataFromSession();
 
     /**
      * Save transaction data to session.
      *
-     * @param mixed $data
+     * @param array $data
      * @return void
      */
     public function saveTransactionDataToSession($data);
+
+    /**
+     * Save the final transaction.
+     *
+     * @param array $data
+     * @return mixed
+     */
+    public function saveTransaction($data);
 }
